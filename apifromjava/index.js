e14bcba9a9e688fe4e0335724c62547426880115
sly <645897796@qq.com>
2018-10-19 14:10:49 +0800

    风控“

--- apifromjava/index.js
+++ apifromjava/index.js
@@ -1,6 +1,6 @@
 import config from 'config-lite';
 const ApiFromJava = {
-	//查询风控规则
+	//调用资管平台接口判断用户是否已登录，以及是否有权限访问该接口
 	checkIsAdmin: config.domain + '/api/asset/positions/1/sub-positions/count'
 }
 
